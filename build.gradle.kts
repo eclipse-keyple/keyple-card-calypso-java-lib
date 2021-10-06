b3cde4df46f11d9ab309307f057214510884a7ee
Andrei Cristea <andrei.cristea019@gmail.com>
2021-10-06 15:13:02 +0200

    Release 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,12 +30,12 @@ repositories {
     maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots")
 }
 dependencies {
-    implementation("org.calypsonet.terminal:calypsonet-terminal-reader-java-api:1.0.0-SNAPSHOT") { isChanging = true }
-    implementation("org.calypsonet.terminal:calypsonet-terminal-card-java-api:1.0.0-SNAPSHOT") { isChanging = true }
-    implementation("org.calypsonet.terminal:calypsonet-terminal-calypso-java-api:1.0.0-SNAPSHOT") { isChanging = true }
-    implementation("org.eclipse.keyple:keyple-common-java-api:2.0.0-SNAPSHOT") { isChanging = true }
-    implementation("org.eclipse.keyple:keyple-service-resource-java-lib:2.0.0-SNAPSHOT") { isChanging = true }
-    implementation("org.eclipse.keyple:keyple-util-java-lib:2.0.0-SNAPSHOT") { isChanging = true }
+    implementation("org.calypsonet.terminal:calypsonet-terminal-reader-java-api:1.0.0")
+    implementation("org.calypsonet.terminal:calypsonet-terminal-card-java-api:1.0.0")
+    implementation("org.calypsonet.terminal:calypsonet-terminal-calypso-java-api:1.0.0")
+    implementation("org.eclipse.keyple:keyple-common-java-api:2.0.0")
+    implementation("org.eclipse.keyple:keyple-service-resource-java-lib:2.0.0")
+    implementation("org.eclipse.keyple:keyple-util-java-lib:2.0.0")
     implementation("org.slf4j:slf4j-api:1.7.32")
     implementation("com.google.code.gson:gson:2.8.8")
     testImplementation("org.slf4j:slf4j-simple:1.7.32")
