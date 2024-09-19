caad2525cd2412f1c97de06ed25996da6e5b5069
Andrei Cristea <andrei.cristea019@gmail.com>
2024-09-19 11:40:08 +0200

    fix: distributed backward compatibility for legacy keyple-less clients

--- src/main/java/org/eclipse/keyple/card/calypso/DtoAdapters.java
+++ src/main/java/org/eclipse/keyple/card/calypso/DtoAdapters.java
@@ -156,6 +156,7 @@ final class DtoAdapters {
 
     private final List<ApduRequestSpi> apduRequests;
     private final boolean stopOnUnsuccessfulStatusWord;
+    private final boolean isStatusCodesVerificationEnabled; // Only for JSON backward compatibility
 
     /**
      * Builds a card request with a list of {@link ApduRequestSpi } and the flag indicating the
@@ -171,6 +172,7 @@ final class DtoAdapters {
     CardRequestAdapter(List<ApduRequestSpi> apduRequests, boolean stopOnUnsuccessfulStatusWord) {
       this.apduRequests = apduRequests;
       this.stopOnUnsuccessfulStatusWord = stopOnUnsuccessfulStatusWord;
+      this.isStatusCodesVerificationEnabled = stopOnUnsuccessfulStatusWord;
     }
 
     /**
